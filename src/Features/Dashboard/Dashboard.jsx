fd01aabe1a365b62abf08911165230417eeda07b
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-04-19 10:35:37 +0330

    feat: develop dashboard component

---
+++ src/Features/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "@/Containers/DashboardLayout";
+
+const Dashboard = () => {
+    return ( 
+        <DashboardLayout>
+            dd
+        </DashboardLayout>
+     );
+}
+ 
+export default Dashboard;
---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,8 @@
+import DashboardFeature from '@/Features/Dashboard/Dashboard' 
+const Dashboard = () => {
+    return ( 
+       <DashboardFeature />
+     );
+}
+ 
+export default Dashboard;
