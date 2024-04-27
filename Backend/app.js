6e7db1483d26502debeed021d06debf54b6695a0
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-04-27 12:37:05 +0330

    feat: add backend of project

---
+++ Backend/app.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const express = require("express");
+const { setHeaders } = require("./middlewares/headers");
+const { errorHandler } = require("./middlewares/errors");
+
+//*routes import
+const usersRoutes = require("./routes/api/user");
+const articlesRoutes = require("./routes/api/article");
+const authRoutes = require("./routes/api/auth");
+const contactRoutes = require("./routes/api/contact");
+const searchRoutes = require("./routes/api/search");
+const ordersRoutes = require("./routes/api/order");
+
+const app = express();
+
+//* BodyPaser
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+
+//* CORS Policy Definitions
+app.use(setHeaders);
+
+//* Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+//* Routes
+app.use("/api/articles", articlesRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/contact", contactRoutes);
+app.use("/api/orders", ordersRoutes);
+app.use("/api/search", searchRoutes);
+app.use("/api/users", usersRoutes);
+
+//* Error Controller
+app.use((req, res) => {
+  console.log("this path is not available:", req.path);
+  res.status(404).json({ message: "404 OOPS! PATH NOT FOUND" });
+});
+app.use(errorHandler);
+
+module.exports = app;
---
+++ Backend/controllers/api/user.js
@@ -0,0 +1,130 @@
+const userModel = require("../../models/user");
+const bcrypt = require("bcrypt");
+
+exports.getAll = async (req, res, next) => {
+  try {
+    const users = await userModel.find();
+
+    return res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.editUser = async (req, res, next) => {
+  try {
+    const { name, username, email, password, phone, role } = req.body;
+    const { id } = req.params;
+    await userModel.editUserValidation({ ...req.body, id }).catch((err) => {
+      err.statusCode = 400;
+      throw err;
+    });
+
+    const hashedPassword = password
+      ? await bcrypt.hash(password, 12)
+      : undefined;
+
+    const updatedUser = await userModel.findByIdAndUpdate(
+      id,
+      {
+        name,
+        username,
+        email,
+        password: hashedPassword,
+        phone,
+        role,
+      },
+      { new: true }
+    );
+
+    return res.json({ user: updatedUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.removeUser = async (req, res, next) => {
+  try {
+    await userModel.removeUserValidation(req.params).catch((err) => {
+      err.statusCode = 400;
+      throw err;
+    });
+
+    const deletedUser = await userModel.findOneAndRemove({
+      _id: req.params.id,
+    });
+
+    if (!deletedUser) {
+      return res.status(404).json("There is not user");
+    }
+
+    return res.status(200).json("User Deleted Successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+exports.getUserCourses = async (req, res, next) => {
+  try {
+    const userCourses = await courseUserModel
+      .find({ user: req.user._id })
+      .populate("course")
+      .lean();
+
+    res.json(userCourses);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateUser = async (req, res, next) => {
+  try {
+    await userModel.updateUserValidation(req.body).catch((err) => {
+      err.statusCode = 400;
+      throw err;
+    });
+
+    const { name, username, email, password, phone } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await userModel.findOneAndUpdate(
+      { _id: req.user._id },
+      {
+        name,
+        username,
+        email,
+        password: hashedPassword,
+        phone,
+      }
+    );
+
+    return res.json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.changeUserRole = async (req, res, next) => {
+  try {
+    await userModel.changeUserRoleValidation(req.body).catch((err) => {
+      err.statusCode = 400;
+      throw err;
+    });
+
+    const { role, id } = req.body;
+    console.log(role , id);
+
+    const user = await userModel.findByIdAndUpdate(
+      { _id: id },
+      {
+        role: role,
+      }
+    );
+
+    res.json({ msg: `User role changed to ${role} successfully` });
+  } catch (error) {
+    next(error);
+  }
+};
