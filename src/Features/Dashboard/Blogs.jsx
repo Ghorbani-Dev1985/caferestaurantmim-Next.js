ade6e6678fef062ab5f1e3c00df1b5bbb79b062e
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-05-01 21:43:21 +0330

    feat: develop pagination in blogs page dashboard

--- src/Features/Dashboard/Blogs.jsx
+++ src/Features/Dashboard/Blogs.jsx
@@ -1,6 +1,5 @@
 import DashboardLayout from "@/Containers/DashboardLayout";
-import React, { useMemo, useState } from "react";
-import BlogsRow from "./BlogsRow";
+import React, { useCallback, useMemo, useState } from "react";
 import Alert from "@/UI/Alert";
 import {
   Table,
@@ -8,22 +7,132 @@ import {
   TableColumn,
   TableBody,
   Pagination,
-  Spinner,
   TableRow,
   TableCell,
-  getKeyValue,
+  Chip,
 } from "@nextui-org/react";
+import Http from "@/Services/HttpService";
+import { useRouter } from "next/router";
+import RouterPush from "@/Hooks/RouterPush";
+import Image from "next/image";
+import ModalPlacement from "@/UI/ModalPlacement";
+import { BiShow } from "react-icons/bi";
+import DOMPurify from "isomorphic-dompurify";
+import { IoCheckmarkCircleSharp } from "react-icons/io5";
+import { RiDraftFill } from "react-icons/ri";
+import ConfirmModal from "@/UI/ConfimModal";
+import { HiOutlineTrash } from "react-icons/hi2";
 
 const BlogsList = ({ blogsList }) => {
   const [page, setPage] = useState(1);
   const rowsPerPage = 10;
   const pages = Math.ceil(blogsList.length / rowsPerPage);
-  const blogs = React.useMemo(() => {
+  const blogs = useMemo(() => {
    const start = (page - 1) * rowsPerPage;
    const end = start + rowsPerPage;
    return blogsList.slice(start, end);
  }, [page, blogsList]);
- console.log(blogs)
+ const router = useRouter();
+ const PublishBlogHandler = async (id) => {
+   await Http.put("/articles", { id })
+   .then(({ data }) => {
+     toast.success(data.message);
+       RouterPush(router);
+     })
+     .catch((err) => console.log(err));
+   };
+   const DeleteBlogHandler = async (id) => {
+     await Http.delete(`/articles/${id}`)
+     .then(({ data }) => {
+       toast.success(data.message);
+       RouterPush(router);
+     })
+     .catch((err) => toast.error(err.message));
+ };
+ const renderCell = useCallback((blog, columnKey) => {
+   const cellValue = blog[columnKey];
+   switch (columnKey) {
+     case "cover":
+       return (
+         <Image
+         width={100}
+         height={100}
+         alt="ghorbani-dev.ir"
+         src={`${process.env.NEXT_PUBLIC_DOMAINAPI_URL}${blog.cover}`}
+         className="object-fill rounded-lg"
+       />
+       );
+     case "title":
+       return (
+       
+           blog.title
+       
+       );
+     case "shortName":
+       return (
+          blog.shortName    
+       );
+        case "description":
+       return (
+         <ModalPlacement
+         icon={<BiShow className="size-8 fill-sky-500" />}
+         title="توضیحات"
+       >
+         {blog.description}
+       </ModalPlacement>   
+       );
+       case "body" :
+         return (
+           <ModalPlacement
+         icon={<BiShow className="size-8 fill-sky-500" />}
+         title="بدنه"
+       >
+           <div
+               dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(blog.body) }}
+             ></div>
+         {blog.body}
+       </ModalPlacement>  
+         );
+      case "publish" :
+         return (
+           blog.publish ? (
+             <Chip
+               startContent={<IoCheckmarkCircleSharp size={18} />}
+               variant="faded"
+               color="success"
+               className="border border-emerald-500"
+             >
+               منتشر شده
+             </Chip>
+           ) : (
+             <Chip
+               startContent={<RiDraftFill size={18} />}
+               variant="faded"
+               color="warning"
+               className="border border-amber-500 cursor-pointer"
+               onClick={() => PublishBlogHandler(blog._id)}
+             >
+               پیش نویس
+             </Chip>
+           )
+         );
+         case "act" :
+         return (
+           <ConfirmModal
+           btnIcon={<HiOutlineTrash className="size-5" />}
+           confirmBtnText="حذف"
+           titleText="حذف مقاله"
+           confirmBtnHandler={() => DeleteBlogHandler(blog._id)}
+         >
+           آیا از حذف مقاله با عنوان
+           {/* <span className="text-sky-500 mx-1">{blog.title}</span> مطمعن هستید؟ */}
+         </ConfirmModal>
+           
+         );
+     default:
+       return cellValue;
+   }
+ }, []);
   return (
     <DashboardLayout>
       {blogsList.length ? (
@@ -31,6 +140,8 @@ const BlogsList = ({ blogsList }) => {
           aria-label="Example table with client async pagination"
           bottomContent={
               <div className="flex w-full justify-center">
+                {
+                  pages > 1 &&
                 <Pagination
                   isCompact
                   showControls
@@ -39,15 +150,21 @@ const BlogsList = ({ blogsList }) => {
                   page={page}
                   total={pages}
                   onChange={(page) => setPage(page)}
+                  classNames={{
+                    prev: "rotate-180",
+                    next: "rotate-180",
+                    forwardIcon: "rotate-180",
+                  }}
                 />
+                }
               </div>
           }
           classNames={{
             wrapper: "min-h-[222px]",
+            th : "first:rounded-tl-none first:rounded-bl-none first:rounded-tr-lg first:rounded-br-lg last:rounded-tr-none last:rounded-br-none last:rounded-tl-lg last:rounded-bl-lg",
           }}
         >
           <TableHeader>
-            <TableColumn key="index">ردیف</TableColumn>
             <TableColumn key="cover">تصویر</TableColumn>
             <TableColumn key="title">عنوان</TableColumn>
             <TableColumn key="shortName">لینک</TableColumn>
@@ -56,25 +173,16 @@ const BlogsList = ({ blogsList }) => {
             <TableColumn key="publish"> وضعیت انتشار</TableColumn>
             <TableColumn key="act"> عملیات</TableColumn>
           </TableHeader>
+         
           <TableBody
            items={blogs}
           >
-            {
-               console.log(blogs)             
-               // blogs.map(({blog , index}) => {
-               //    return(
-               //       <React.Fragment key={blog._id}>
-               //          <BlogsRow blog={blog} index={index}/>
-               //       </React.Fragment>
-               //    )
-               // })
-            }
-            {/* {(blog, index) => (
+     {(blog) => (
               <TableRow key={blog._id}>
-                 {(columnKey) => {<TableCell>{getKeyValue(blog, columnKey)}</TableCell>}}
+                 {(columnKey) => <TableCell>{renderCell(blog, columnKey)}</TableCell>}
                 
               </TableRow>
-            )} */}
+            )}
           </TableBody>
         </Table>
       ) : (
--- src/Features/Dashboard/BlogsRow.jsx
+++ src/Features/Dashboard/BlogsRow.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Table from "@/UI/Table";
-import { Button, Chip, TableCell, TableRow } from "@nextui-org/react";
+import { Button, Chip, TableBody, TableCell, TableRow, getKeyValue } from "@nextui-org/react";
 import Image from "next/image";
 import { IoCheckmarkCircleSharp } from "react-icons/io5";
 import { RiDraftFill } from "react-icons/ri";
@@ -13,76 +13,77 @@ import toast from "react-hot-toast";
 import { useRouter } from "next/router";
 import RouterPush from "@/Hooks/RouterPush";
 import DOMPurify from "isomorphic-dompurify";
-const BlogsRow = ({ blog, index }) => {
-  console.log(blog)
+const BlogsRow = ({ blogs }) => {
   const router = useRouter();
-  const { _id, body, cover, description, publish, shortName, title } = blog;
-  const src = `${process.env.NEXT_PUBLIC_DOMAINAPI_URL}${cover}`;
   const PublishBlogHandler = async (id) => {
     await Http.put("/articles", { id })
-      .then(({ data }) => {
-        toast.success(data.message);
+    .then(({ data }) => {
+      toast.success(data.message);
         RouterPush(router);
       })
       .catch((err) => console.log(err));
-  };
-  const DeleteBlogHandler = async (id) => {
-    await Http.delete(`/articles/${id}`)
+    };
+    const DeleteBlogHandler = async (id) => {
+      await Http.delete(`/articles/${id}`)
       .then(({ data }) => {
         toast.success(data.message);
         RouterPush(router);
       })
       .catch((err) => toast.error(err.message));
   };
-  return (
-    <>
-       <TableRow>
-        <TableCell>{+index + 1}</TableCell>
-         <TableCell>
-        <Image
-              width={100}
-              height={100}
-              alt="ghorbani-dev.ir"
-              src={src}
-              className="object-fill rounded-lg"
-            />
-        </TableCell> 
-       </TableRow>
-      
-      {/* <Table.Row>
-        <tr className="bg-secondary-50/50 text-center border-b last:border-b-0 text-secondary-700 hover:bg-slate-100 dark:hover:bg-secondary-500 px-6 py-4 transition-colors">
-          <th className="px-6 py-4">{index + 1}</th>
-          <td className="px-6 py-4">
-            <Image
-              width={100}
-              height={100}
-              alt="ghorbani-dev.ir"
-              src={src}
-              className="object-fill rounded-lg"
-            />
-          </td>
-          <td className="px-6 py-4">{title}</td>
-          <td className="px-6 py-4">{shortName}</td>
-          <td>
+  const renderCell = useCallback((blog, columnKey) => {
+    const cellValue = blog[columnKey];
+
+    let index = 0;
+    switch (columnKey) {
+      case "index":
+        return (
+          +index + 1
+        );
+      case "cover":
+        return (
+          <Image
+          width={100}
+          height={100}
+          alt="ghorbani-dev.ir"
+          src={`${process.env.NEXT_PUBLIC_DOMAINAPI_URL}${cellValue.cover}`}
+          className="object-fill rounded-lg"
+        />
+        );
+      case "title":
+        return (
+        
+            cellValue.title
+        
+        );
+      case "shortName":
+        return (
+           cellValue.shortName    
+        );
+         case "description":
+        return (
+          <ModalPlacement
+          icon={<BiShow className="size-8 fill-sky-500" />}
+          title="توضیحات"
+        >
+          {cellValue.description}
+        </ModalPlacement>   
+        );
+        case "body" :
+          return (
             <ModalPlacement
-              icon={<BiShow className="size-8 fill-sky-500" />}
-              title="توضیحات"
-            >
-              {description}
-            </ModalPlacement>
-          </td>
-          <td>
-            <ModalPlacement
-              icon={<BiShow className="size-8 fill-sky-500" />}
-              title="بدنه"
-            >
-              <div
-                dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(body) }}
+          icon={<BiShow className="size-8 fill-sky-500" />}
+          title="بدنه"
+        >
+            <div
+                dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(cellValue.body) }}
               ></div>
-            </ModalPlacement>
-          </td>
-          <td className="px-6 py-4">
-            {publish ? (
+          {cellValue.body}
+        </ModalPlacement>  
+          );
+       case "publish" :
+          return (
+            cellValue.publish ? (
               <Chip
                 startContent={<IoCheckmarkCircleSharp size={18} />}
                 variant="faded"
@@ -97,25 +98,42 @@ const BlogsRow = ({ blog, index }) => {
                 variant="faded"
                 color="warning"
                 className="border border-amber-500 cursor-pointer"
-                onClick={() => PublishBlogHandler(_id)}
+                onClick={() => PublishBlogHandler(cellValue._id)}
               >
                 پیش نویس
               </Chip>
-            )}
-          </td>
-          <td>
+            )
+          );
+          case "act" :
+          return (
             <ConfirmModal
-              btnIcon={<HiOutlineTrash className="size-5" />}
-              confirmBtnText="حذف"
-              titleText="حذف مقاله"
-              confirmBtnHandler={() => DeleteBlogHandler(_id)}
-            >
-              آیا از حذف مقاله با عنوان{" "}
-              <span className="text-sky-500 mx-1">{title}</span> مطمعن هستید؟
-            </ConfirmModal>
-          </td>
-        </tr>
-      </Table.Row> */}
+            btnIcon={<HiOutlineTrash className="size-5" />}
+            confirmBtnText="حذف"
+            titleText="حذف مقاله"
+            confirmBtnHandler={() => DeleteBlogHandler(cellValue._id)}
+          >
+            آیا از حذف مقاله با عنوان{" "}
+            <span className="text-sky-500 mx-1">{cellValue.title}</span> مطمعن هستید؟
+          </ConfirmModal>
+            
+          );
+      default:
+        return cellValue;
+    }
+  }, []);
+  return (
+    <>
+     <TableBody
+           items={blogs}
+          >
+     {(blog, index) => (
+              <TableRow key={blog._id}>
+                 {(columnKey) => <TableCell>{renderCell(blog, columnKey)}</TableCell>}
+                
+              </TableRow>
+            )}
+          </TableBody>
+  
     </>
   );
 };
