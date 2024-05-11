61b239624fa18de2099e5de92ac9cd72e209cf65
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-05-11 18:32:47 +0330

    feat: add googletagmanager

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,34 @@
-
 import { NextUIProvider } from "@nextui-org/react";
-import '@/Styles/globals.css'
+import "@/Styles/globals.css";
 import { Toaster } from "react-hot-toast";
 import AuthProvider from "src/Context/AuthContext";
+import Script from "next/script";
 export default function App({ Component, pageProps }) {
-  return (<AuthProvider><NextUIProvider> <Toaster /><Component {...pageProps} /></NextUIProvider></AuthProvider>)
+  return (
+    <>
+     <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+      />
+       <Script
+        id="gtag-init"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+          `,
+        }}
+      />
+      <AuthProvider>
+        <NextUIProvider>
+          <Toaster />
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </AuthProvider>
+    </>
+  );
 }
